7db9fde74e0235f90bbef0a43ca81555e0719acc
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2025-02-07 10:52:04 +0100

    comm

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -55,7 +55,7 @@ router.get('/github', passport.authenticate('github', { scope: ['user:email'] })
 // Use oauthCallback from the oauth2.js file
 router.get(
   'auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  passport.authenticate('github', { failureRedirect: 'https://github.com/login/oauth/authorize' }),
   oauthCallback
 );
 // Logout route
